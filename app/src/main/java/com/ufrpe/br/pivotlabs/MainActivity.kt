af99347895f0e4f2f82b922b0e7617086e881cc0
IsmaelCesar <leamscesar@gmail.com>
2019-06-14 14:17:55 -0300

    Tela principal
    Tela principal, com botôes para login de usuario

---
+++ app/src/main/java/com/ufrpe/br/pivotlabs/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ufrpe.br.pivotlabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ufrpe.br.pivotlabs.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ufrpe/br/pivotlabs/QuemSomosActivity.kt
@@ -0,0 +1,12 @@
+package com.ufrpe.br.pivotlabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuemSomosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quem_somos)
+    }
+}
