d8d9042a51a4d8c203c09c1e2d8580cf7c23b608
IsmaelCesar <leamscesar@gmail.com>
2019-06-14 15:33:06 -0300

    fixing main activity

--- app/src/main/java/com/ufrpe/br/pivotlabs/MainActivity.kt
+++ app/src/main/java/com/ufrpe/br/pivotlabs/MainActivity.kt
@@ -2,12 +2,15 @@ package com.ufrpe.br.pivotlabs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.ufrpe.br.pivotlabs.R
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var imgBtnQuemSomos : ImageButton
+    lateinit var imgBtnContato : ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
