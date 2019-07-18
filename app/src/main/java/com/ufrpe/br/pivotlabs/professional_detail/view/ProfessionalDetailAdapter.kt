4d57ee7d28c9faa9ac310152dfdfbf336a81e3fb
lucas <lfpgc96@hotmail.com>
2019-07-18 18:44:49 -0300

    Marcação de consultas funcional

--- app/src/main/java/com/ufrpe/br/pivotlabs/professional_detail/view/ProfessionalDetailAdapter.kt
+++ app/src/main/java/com/ufrpe/br/pivotlabs/professional_detail/view/ProfessionalDetailAdapter.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ufrpe.br.pivotlabs.R
+import com.ufrpe.br.pivotlabs.ScheduleActivity
 import com.ufrpe.br.pivotlabs.beans.Schedule
+import kotlinx.android.synthetic.main.professional_schedule_item.view.*
 
-class ProfessionalDetailAdapter(var items: ArrayList<Schedule>, var context: Context)
-    : RecyclerView.Adapter<ProfessionalDetailAdapter.ViewHolder>() {
+class ProfessionalDetailAdapter(var items: ArrayList<Schedule>, var context: Context) :
+    RecyclerView.Adapter<ProfessionalDetailAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.professional_schedule_item, parent, false)
@@ -21,10 +23,19 @@ class ProfessionalDetailAdapter(var items: ArrayList<Schedule>, var context: Con
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.startTime.text = items[position].start
+        holder.finish.text = items[position].finish
+        holder.day.text = items[position].day
 
+        holder.btn.setOnClickListener {
+            (context as ScheduleActivity).mark(position + 1)
+        }
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var btn = view.btnSelectHour
+        var startTime = view.tvStartTime
+        var finish = view.tvFinishTime
+        var day = view.tvDay
     }
 }
--- app/src/main/java/com/ufrpe/br/pivotlabs/professional_select/view/ProfessionalAdapter.kt
+++ app/src/main/java/com/ufrpe/br/pivotlabs/professional_select/view/ProfessionalAdapter.kt
@@ -1,15 +1,18 @@
 package com.ufrpe.br.pivotlabs.professional_select.view
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ufrpe.br.pivotlabs.R
+import com.ufrpe.br.pivotlabs.ScheduleActivity
 import com.ufrpe.br.pivotlabs.beans.Doctor
 import kotlinx.android.synthetic.main.professional_item.view.*
 
-class ProfessionalAdapter(var items: ArrayList<Doctor>, var context: Context): RecyclerView.Adapter<ProfessionalAdapter.ViewHolder>() {
+class ProfessionalAdapter(var items: ArrayList<Doctor>, var context: Context) :
+    RecyclerView.Adapter<ProfessionalAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.professional_item, parent, false)
@@ -23,12 +26,20 @@ class ProfessionalAdapter(var items: ArrayList<Doctor>, var context: Context): R
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.tvProfessionalName.text = items[position].name
         holder.tvSpeciality.text = items[position].speciality
+        holder.item.setOnClickListener {
+            var intent = Intent(context, ScheduleActivity::class.java)
+            intent.putExtra("professional_name", items[position].name)
+            context.startActivity(intent)
+        }
+
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val item = view.cl_prof_item
         val tvProfessionalName = view.tvProfessionalName
         val tvDayPeriod = view.tvDayPeriod
         val tvSpeciality = view.tvSpeciality
+
     }
 
 }
